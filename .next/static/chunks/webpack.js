65ddb14d105e54a72b38dfc52d8143496eac0801
botdoc1 <botdoc4@gmail.com>
2025-10-02 21:42:27 +0300

    Added f2b4f47acfaa8378.webpack.hot-update.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f2b4f47acfaa8378"; }
+/******/ 		__webpack_require__.h = function() { return "823ca28599855de7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
